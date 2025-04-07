37229227592f71e8e795c146d97ac525070d5ef9
Mitchell Harrington <mjh096@student.usc.edu.au>
2025-04-07 14:34:21 +1000

    Created Unit Tests for getArea(), getPerimeter() and toString() methods (5)

--- src/test/java/RectBedTest.java
+++ src/test/java/RectBedTest.java
@@ -18,4 +18,23 @@ public class RectBedTest {
         assertEquals(6, rBed.getArea());
     }
 
+    @Test
+    void testGetPerimeter() {
+        RectBed rBed = new RectBed();
+
+        rBed.setWidth(4);
+        rBed.setHeight(3);
+        assertEquals(14, rBed.getPerimeter());
+    }
+
+    @Test
+    void testToString() {
+        RectBed rBed = new RectBed();
+
+        rBed.setLeft(1);
+        rBed.setTop(1);
+        rBed.setWidth(3);
+        rBed.setHeight(4);
+        assertEquals("Rectangle 1.00 1.00 3.00 4.00", rBed.toString());
+    }
 }
