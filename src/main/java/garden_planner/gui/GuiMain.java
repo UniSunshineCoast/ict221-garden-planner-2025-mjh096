c91eae23311b82b9b9f50e8241f4a73b19178d76
Mitchell Harrington <mjh096@student.usc.edu.au>
2025-04-07 15:16:21 +1000

    Added GardenPlanner (Model) to GuiMain.java (6)

--- src/main/java/garden_planner/gui/GuiMain.java
+++ src/main/java/garden_planner/gui/GuiMain.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -19,14 +20,38 @@ import javafx.stage.Stage;
  */
 public class GuiMain extends Application {
 
+    private GardenPlanner planner;
+
+    public GuiMain() {
+        planner = new GardenPlanner();
+        planner.createBasicDesign(); // Setting up default beds
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         // Parent root = FXMLLoader.load(getClass().getResource("garden_gui.fxml"));
-        Button root = new Button("TODO: design garden planner GUI");
+
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 800, 600);
+        final int SCALE = 100;
+
+        for (RectBed bed : planner.getBeds()) {
+            Rectangle rect = new Rectangle();
+
+            rect.setX(bed.getLeft() * SCALE);
+            rect.setY(bed.getTop() * SCALE);
+            rect.setWidth(bed.getWidth() * SCALE);
+            rect.setHeight(bed.getHeight() * SCALE);
+
+            rect.setFill(Color.LIGHTBLUE);
+            root.getChildren().add(rect);
+        }
+
+        root.setStyle("-fx-background-color: #007700;");
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
+        primaryStage.setScene(scene);
+        primaryStage.show(); // Display scene
+
     }
 
 
