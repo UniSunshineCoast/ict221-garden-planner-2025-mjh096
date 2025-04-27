51eef13e07174230d74e3ec850c3e22c34b14f06
Mitchell Harrington <ya-hell@hotmail.com>
2025-04-27 20:14:51 +1000

    Updated for Lab 7, Updated Controller.java and Changed Gradle config to include resources folder.

--- src/main/java/garden_planner/gui/Controller.java
+++ src/main/java/garden_planner/gui/Controller.java
@@ -1,11 +1,17 @@
 package garden_planner.gui;
 
+import garden_planner.model.CircleBed;
 import garden_planner.model.GardenBed;
 import garden_planner.model.GardenPlanner;
 import garden_planner.model.RectBed;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class Controller {
@@ -27,11 +33,20 @@ public class Controller {
     private TextField perimeter;
 
     @FXML
+    private TextArea summary;
+
+    @FXML
+    private Button addRect;
+
+    @FXML
+    private Button addCircle;
+
+    @FXML
     public void initialize() {
         planner = new GardenPlanner();
         planner.createBasicDesign();
 
-        garden.setStyle("-fx-background-color: #007700;");
+        garden.setStyle("-fx-background-color: #007700; -fx-background-image: url(\"grass.png\")");
 
         updateGUI();
         RectBed firstRect = (RectBed) planner.getBeds().get(0);
@@ -52,6 +67,16 @@ public class Controller {
 
             updateGUI();
         });
+
+        addRect.setOnAction(e -> {
+            planner.getBeds().add(new RectBed());
+            updateGUI();
+        });
+
+        addCircle.setOnAction(e -> {
+            planner.getBeds().add(new CircleBed(1.0));
+            updateGUI();
+        });
     }
 
     public void updateGUI() {
@@ -59,15 +84,62 @@ public class Controller {
         // A default garden layout is created in the constructor
         garden.getChildren().clear();
         for (GardenBed bed : planner.getBeds()) {
-            Rectangle rec = new Rectangle(bed.getWidth()*100, bed.getHeight()*100);
-            rec.setX(bed.getLeft()*100);
-            rec.setY(bed.getTop()*100);
-            garden.getChildren().add(rec);
+            if (bed instanceof RectBed) {
+                Rectangle rect = new Rectangle(bed.getWidth() * 100, bed.getHeight() * 100);
+                rect.setX(bed.getLeft()*100);
+                rect.setY(bed.getTop()*100);
+
+                Image map = new Image("vege.jpg");
+                ImagePattern pattern = new ImagePattern(map, 20, 20, 40, 40, false);
+                rect.setFill(pattern);
+
+                rect.setOnMouseDragged(ev -> {
+                    // System.out.println("x=" + ev.getX() + " r=" + bed);
+                    rect.setX(ev.getX());
+                    rect.setY(ev.getY());
+                    bed.setLeft(ev.getX() / 100); // scale is 100 to adjust the display
+                    bed.setTop(ev.getY() / 100);
+                });
+
+                garden.getChildren().add(rect);
+            } else if (bed instanceof CircleBed) {
+                double radius = ((CircleBed) bed).getRadius();
+                Circle cir = new Circle(radius * 100);
+                cir.setCenterX((bed.getLeft()+radius)*100);
+                cir.setCenterY((bed.getTop()+radius)*100);
+
+                Image map = new Image("flowers.jpg");
+                ImagePattern pattern = new ImagePattern(map, 20, 20, 40, 40, false);
+                cir.setFill(pattern);
+
+                cir.setOnMouseDragged(ev -> {
+                    // System.out.println("x=" + ev.getX() + " r=" + bed);
+                    cir.setCenterX(ev.getX());
+                    cir.setCenterY(ev.getY());
+                    bed.setLeft(ev.getX() / 100); // scale is 100 to adjust the display
+                    bed.setTop(ev.getY() / 100);
+                });
+
+                garden.getChildren().add(cir);
+            }
         }
 
         RectBed firstRect = (RectBed) planner.getBeds().get(0);
         area.setText(String.valueOf(firstRect.getArea()));
         perimeter.setText(String.valueOf(firstRect.getPerimeter()));
+
+        planner.recalculateTotals();
+        String summaryText = String.format("Total garden area is: %8.2f m2.\n " +
+                "Total wall length is: %8.2f m.\n" +
+                "Total soil required:  %8.2f m3.\n" +
+                "Total garden cost is: $%7.2f.\n",
+                planner.getTotalGardenArea(),
+                planner.getTotalWallLength(),
+                planner.getTotalGardenArea() * planner.SOIL_DEPTH,
+                planner.getTotalCost()
+        );
+
+        summary.setText(summaryText);
 
 
     }
